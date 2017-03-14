18d178fc1eccc9c25a021555ffb347094ced13cb
Alexander Goldberg <algold@gmail.com>
2017-03-14 15:38:19 +0200

    Stateful drain with traits

--- src/drains.rs
+++ src/drains.rs
@@ -1,44 +1,78 @@
-use config::UDSConfig;
-use format::SyslogFormat;
-use slog::{Drain, Level, Record, OwnedKeyValueList, Never};
+// use config::UDSConfig;
+// use format::SyslogFormat;
+use slog::{Drain, Record, OwnedKeyValueList, Never};
 use slog_stream::Format as StreamFormat;
-use std::cell::RefCell;
+use std;
+// use std::cell::RefCell;
 use std::io;
-use std::net::ToSocketAddrs;
 use std::os::unix::net::UnixDatagram;
 use std::path::PathBuf;
-use std;
+use std::net::Shutdown;
 
-pub struct UDSDrain<F>
-    where F: StreamFormat
-{
-    socket: PathBuf,
-    formatter: F,
+#[derive(Default, Debug)]
+pub struct DrainDisconnected {}
+
+#[derive(Debug)]
+pub struct DrainConnected {
+    socket: UnixDatagram,
 }
 
-impl<F> UDSDrain<F>
+#[derive(Debug)]
+pub struct UDSDrain<C, F>
     where F: StreamFormat
 {
-    pub fn new(socket: PathBuf, formatter: F) -> Self {
-        UDSDrain {
-            socket: socket,
+    path_to_socket: PathBuf,
+    formatter: F,
+    connection: C,
+}
+
+impl<F> UDSDrain<DrainDisconnected, F>
+    where F: StreamFormat
+{
+    pub fn new(path_to_socket: PathBuf, formatter: F) -> UDSDrain<DrainDisconnected, F> {
+        UDSDrain::<DrainDisconnected, F> {
+            path_to_socket: path_to_socket,
             formatter: formatter,
+            connection: DrainDisconnected {},
         }
     }
 }
 
-impl<F> Drain for UDSDrain<F>
+impl<F> UDSDrain<DrainDisconnected, F>
+    where F: StreamFormat
+{
+    pub fn connect(self) -> Result<UDSDrain<DrainConnected, F>, io::Error> {
+        let socket = UnixDatagram::unbound()?;
+        Ok(UDSDrain::<DrainConnected, F> {
+            path_to_socket: self.path_to_socket,
+            formatter: self.formatter,
+            connection: DrainConnected { socket: socket },
+        })
+    }
+}
+
+impl<F> UDSDrain<DrainConnected, F>
+    where F: StreamFormat
+{
+    pub fn disconnect(self) -> Result<(), io::Error> {
+        self.connection.socket.shutdown(Shutdown::Both)
+    }
+}
+
+
+impl<F> Drain for UDSDrain<DrainConnected, F>
     where F: StreamFormat
 {
     type Error = Never;
 
-    fn log(&self, info: &Record, logger_values: &OwnedKeyValueList) -> std::result::Result<(), Never> {
+    fn log(&self,
+           info: &Record,
+           logger_values: &OwnedKeyValueList)
+           -> std::result::Result<(), Never> {
 
         let mut buf = Vec::<u8>::with_capacity(4096);
         self.formatter.format(&mut buf, info, logger_values).unwrap();
-
-        let socket = UnixDatagram::unbound().unwrap();
-        socket.send_to(buf.as_slice(), &self.socket).unwrap();
+        self.connection.socket.send_to(buf.as_slice(), &self.path_to_socket).unwrap();
         Ok(())
     }
 }
--- tests/drains.rs
+++ tests/drains.rs
@@ -15,19 +15,60 @@ mod tests {
     use std::sync::{Arc, Mutex};
 
     #[test]
-    fn drain() {
-        let formatter = SyslogFormat::<HeaderRFC3164Minimal,
+    fn uds_drain_rfc3164_minimal() {
+        let formatter = SyslogFormat::<HeaderRFC3164Minimal, MessageKSV>::new(None,
+                                                                              Some("test"
+                                                                                  .to_owned()),
+                                                                              12345,
+                                                                              Facility::LOG_USER);
+
+        let test_drain =
+            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc3164"), formatter).connect().unwrap();
+        // let test_drain = UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter);
+        println!("{:?}", test_drain);
+
+        let logger = Logger::root(test_drain, o!("lk1" => "lv1", "lk2" => "lv2"));
+        info!(logger, "Test message RFC3164 minimal"; "mk1" => "mv1", "mk2" => "mv2" );
+        assert!(false);
+
+    }
+
+    #[test]
+    fn uds_drain_rfc3164_full() {
+        let formatter = SyslogFormat::<HeaderRFC3164<Timestamp<TimestampRFC3164,
+                                                               TimestampLocal>>,
                                        MessageKSV>::new(None,
                                                         Some("test".to_owned()),
                                                         12345,
                                                         Facility::LOG_USER);
 
-        // let test_drain = UDSDrain::new(PathBuf::from("/tmp/rsyslog/log"), formatter);
-        let test_drain = UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter);
+        let test_drain =
+            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc3164"), formatter).connect().unwrap();
+        // let test_drain = UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter);
 
         let logger = Logger::root(test_drain, o!("lk1" => "lv1", "lk2" => "lv2"));
-        info!(logger, "Test message 1"; "mk1" => "mv1", "mk2" => "mv2" );
+        info!(logger, "Test message RFC3164 full"; "mk1" => "mv1", "mk2" => "mv2" );
         assert!(false);
 
     }
+
+    #[test]
+    fn uds_drain_rfc5424() {
+        let formatter = SyslogFormat::<HeaderRFC5424<Timestamp<TimestampISO8601,
+                                                               TimestampLocal>>,
+                                       MessageRFC5424>::new(None,
+                                                            Some("test".to_owned()),
+                                                            12345,
+                                                            Facility::LOG_USER);
+
+        let test_drain =
+            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc5424"), formatter).connect().unwrap();
+        // let test_drain = UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter);
+
+        let logger = Logger::root(test_drain, o!("lk1" => "lv1", "lk2" => "lv2"));
+        info!(logger, "Test message RFC5424"; "mk1" => "mv1", "mk2" => "mv2" );
+        assert!(false);
+
+    }
+
 }
