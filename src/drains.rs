58c5695172eeb75ef84d31ec4dc09398fe86068d
Alexander Goldberg <algold@gmail.com>
2017-03-14 16:23:31 +0200

    Correct Error Type

--- src/drains.rs
+++ src/drains.rs
@@ -1,19 +1,20 @@
 // use config::UDSConfig;
 // use format::SyslogFormat;
-use slog::{Drain, Record, OwnedKeyValueList, Never};
+
+use slog::{Drain, Record, OwnedKeyValueList};
 use slog_stream::Format as StreamFormat;
 use std;
 // use std::cell::RefCell;
 use std::io;
+use std::net::Shutdown;
 use std::os::unix::net::UnixDatagram;
 use std::path::PathBuf;
-use std::net::Shutdown;
 
 #[derive(Default, Debug)]
-pub struct DrainDisconnected {}
+pub struct Disconnected {}
 
 #[derive(Debug)]
-pub struct DrainConnected {
+pub struct Connected {
     socket: UnixDatagram,
 }
 
@@ -26,53 +27,64 @@ pub struct UDSDrain<C, F>
     connection: C,
 }
 
-impl<F> UDSDrain<DrainDisconnected, F>
+impl<F> UDSDrain<Disconnected, F>
     where F: StreamFormat
 {
-    pub fn new(path_to_socket: PathBuf, formatter: F) -> UDSDrain<DrainDisconnected, F> {
-        UDSDrain::<DrainDisconnected, F> {
+    pub fn new(path_to_socket: PathBuf, formatter: F) -> UDSDrain<Disconnected, F> {
+        UDSDrain::<Disconnected, F> {
             path_to_socket: path_to_socket,
             formatter: formatter,
-            connection: DrainDisconnected {},
+            connection: Disconnected {},
         }
     }
 }
 
-impl<F> UDSDrain<DrainDisconnected, F>
+impl<F> UDSDrain<Disconnected, F>
     where F: StreamFormat
 {
-    pub fn connect(self) -> Result<UDSDrain<DrainConnected, F>, io::Error> {
+    pub fn connect(self) -> Result<UDSDrain<Connected, F>, io::Error> {
         let socket = UnixDatagram::unbound()?;
-        Ok(UDSDrain::<DrainConnected, F> {
+        Ok(UDSDrain::<Connected, F> {
             path_to_socket: self.path_to_socket,
             formatter: self.formatter,
-            connection: DrainConnected { socket: socket },
+            connection: Connected { socket: socket },
         })
     }
 }
 
-impl<F> UDSDrain<DrainConnected, F>
+impl<F> UDSDrain<Connected, F>
     where F: StreamFormat
 {
-    pub fn disconnect(self) -> Result<(), io::Error> {
+    pub fn disconnect(&mut self) -> Result<(), io::Error> {
         self.connection.socket.shutdown(Shutdown::Both)
     }
 }
 
+// TODO: temporary disabled: https://github.com/rust-lang/rust/issues/38868
+// impl <F>Drop for UDSDrain<Connected, F>
+//     where F: StreamFormat {
+//     fn drop(&mut self) {
+//         self.disconnect();
+//     }
+// }
 
-impl<F> Drain for UDSDrain<DrainConnected, F>
+
+impl<F> Drain for UDSDrain<Connected, F>
     where F: StreamFormat
 {
-    type Error = Never;
+    type Error = io::Error;
 
     fn log(&self,
            info: &Record,
            logger_values: &OwnedKeyValueList)
-           -> std::result::Result<(), Never> {
+           -> std::result::Result<(), io::Error> {
 
+        // Should be thread safe
         let mut buf = Vec::<u8>::with_capacity(4096);
+
         self.formatter.format(&mut buf, info, logger_values).unwrap();
-        self.connection.socket.send_to(buf.as_slice(), &self.path_to_socket).unwrap();
+
+        self.connection.socket.send_to(buf.as_slice(), &self.path_to_socket)?;
         Ok(())
     }
 }
