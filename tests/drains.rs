d6e98376c08d440e41a50ebc8a88a757b7606e38
Alexander Goldberg <algold@gmail.com>
2017-03-16 15:46:39 +0200

    Refactor tests a bit

--- tests/drains.rs
+++ tests/drains.rs
@@ -6,6 +6,9 @@ extern crate slog_term;
 
 use slog_syslog_ng::*;
 
+// TODO: Get IP straight from DOCKER_ENV
+// TODO: Factor out fixtures
+
 #[cfg(test)]
 mod tests {
 
@@ -15,105 +18,82 @@ mod tests {
     use std;
     use std::net::SocketAddr;
     use std::path::PathBuf;
+    use std::str::FromStr;
     use std::sync::{Arc, Mutex};
 
-    #[test]
-    fn uds_drain_rfc3164_minimal() {
-        let formatter = SyslogFormat::<HeaderRFC3164Minimal, MessageKSV>::new(None,
-                                                                              Some("test"
-                                                                                  .to_owned()),
-                                                                              12345,
-                                                                              Facility::LOG_USER);
+    type TsIsoUtc = Timestamp<TimestampISO8601, TimestampUTC>;
+    type TsIosLocal = Timestamp<TimestampISO8601, TimestampLocal>;
+    type Ts3164Utc = Timestamp<TimestampRFC3164, TimestampUTC>;
+    type Ts3164Local = Timestamp<TimestampRFC3164, TimestampLocal>;
+
+    // Formater fixture
+    macro_rules! formatter(
+        ($header: ty, $message: ty) => (
+            SyslogFormat::<$header, $message>::new(
+                None, Some("test" .to_owned()), 12345, Facility::LOG_USER)
+    ));
+
+    // Emit message Fixture
+    macro_rules! logger_emit(
+        ($drain: ident, $header: ty, $message: ty, $dest: expr, $event: expr) => {{
         let console_drain = slog_term::streamer().full().build();
-        let test_drain =
-            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc3164"), formatter).connect().unwrap();
-
-        // let test_drain =
-        //     UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter).connect().unwrap();
-
-        println!("{:?}", test_drain);
-        let logger = Logger::root(duplicate(console_drain, test_drain).fuse(),
-                                  o!("key1" => "value1", "key2" => "value2"));
-
-        info!(logger, "Test message RFC3164 minimal"; "mk1" => "mv1", "mk2" => "mv2" );
-        assert!(false);
-
-    }
-
-    #[test]
-    fn uds_drain_rfc3164_full() {
-        let formatter = SyslogFormat::<HeaderRFC3164<Timestamp<TimestampRFC3164,
-                                                               TimestampLocal>>,
-                                       MessageKSV>::new(None,
-                                                        Some("test".to_owned()),
-                                                        12345,
-                                                        Facility::LOG_USER);
-
-        let console_drain = slog_term::streamer().full().build();
-        let test_drain =
-            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc3164"), formatter).connect().unwrap();
-
-        // let test_drain =
-        //     UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter).connect().unwrap();
-
-        println!("{:?}", test_drain);
-
-        let logger = Logger::root(duplicate(console_drain, test_drain).fuse(),
-                                  o!("key1" => "value1", "key2" => "value2"));
-
-        info!(logger, "Test message RFC3164 full"; "mk1" => "mv1", "mk2" => "mv2" );
-        assert!(false);
-
-    }
-
-    #[test]
-    fn uds_drain_rfc5424() {
-        let formatter = SyslogFormat::<HeaderRFC5424<Timestamp<TimestampISO8601,
-                                                               TimestampLocal>>,
-                                       MessageRFC5424>::new(None,
-                                                            Some("test".to_owned()),
-                                                            12345,
-                                                            Facility::LOG_USER);
-
-        let console_drain = slog_term::streamer().full().build();
-        let test_drain =
-            UDSDrain::new(PathBuf::from("/tmp/rsyslog/rfc3164"), formatter).connect().unwrap();
-
-        // let test_drain =
-        //     UDSDrain::new(PathBuf::from("/var/run/syslog"), formatter).connect().unwrap();
-
-        println!("{:?}", test_drain);
-
-        let logger = Logger::root(duplicate(console_drain, test_drain).fuse(),
-                                  o!("key1" => "value1", "key2" => "value2"));
-
-        info!(logger, "Test message RFC5424"; "mk1" => "mv1", "mk2" => "mv2" );
-        assert!(false);
-
-    }
-
-    #[test]
-    fn udp_drain_rfc3164_minimal() {
-        let formatter = SyslogFormat::<HeaderRFC3164Minimal, MessageKSV>::new(None,
-                                                                              Some("test"
-                                                                                  .to_owned()),
-                                                                              12345,
-                                                                              Facility::LOG_USER);
-
-        let console_drain = slog_term::streamer().full().build();
-        let test_drain = UDPDrain::new(SocketAddr::from_str("192.168.99.100:10514").unwrap(),
-                                       formatter)
+        let test_drain = $drain::new($dest, formatter!($header, $message))
             .connect()
             .unwrap();
 
         println!("{:?}", test_drain);
         let logger = Logger::root(duplicate(console_drain, test_drain).fuse(),
-                                  o!("key1" => "value1", "key2" => "value2"));
+                                  o!("lk1" => "lv1", "lk2" => "lv2"));
+            info!(logger, $event; "mk1" => "mv1", "mk2" => "mv2")
+        }});
 
-        info!(logger, "Test message RFC3164 minimal"; "mk1" => "mv1", "mk2" => "mv2" );
+    #[test]
+    fn uds_drain_rfc3164_minimal() {
+        let dest = PathBuf::from("/tmp/syslog/rfc3164");
+        logger_emit!(UDSDrain,
+                     HeaderRFC3164Minimal,
+                     MessageKSV,
+                     dest,
+                     "Test message RFC3164 minimal");
         assert!(false);
-
     }
 
+    #[test]
+    fn uds_drain_rfc3164_full() {
+        let dest = PathBuf::from("/tmp/syslog/rfc3164");
+        logger_emit!(
+            UDSDrain,
+            HeaderRFC3164<Ts3164Local>,
+            MessageKSV,
+            dest,
+            "Test message RFC3164 full"
+        );
+        assert!(false);
+    }
 
+    #[test]
+    fn uds_drain_rfc5424() {
+        let dest = PathBuf::from("/tmp/syslog/rfc5424");
+        logger_emit!(
+            UDSDrain,
+            HeaderRFC3164<TsIosLocal>,
+            MessageRFC5424,
+            dest,
+            "Test message RFC5424 Native"
+        );
+        assert!(false);
+    }
+
+    #[test]
+    fn udp_drain_rfc3164_minimal() {
+        let dest = SocketAddr::from_str("192.168.99.100:10514").unwrap();
+        logger_emit!(
+            UDPDrain,
+            HeaderRFC3164Minimal,
+            MessageKSV,
+            dest,
+            "Test message RFC3164 minimal"
+        );
+        assert!(false);
+    }
 }
